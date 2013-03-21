e1be5ca44c5118dd7aed5ed2e215681438547fa6
Marina Thuns <mthuns@Marinas-MacBook-Pro.local>
2013-03-21 22:14:17 +0100

    bevor den gruppen

--- wp-content/themes/klimotion_theme/script/klimo_localgroupspage.js
+++ wp-content/themes/klimotion_theme/script/klimo_localgroupspage.js
@@ -7,57 +7,15 @@ jQuery(function($) { klimo_localgroupspage : {
 
 		function Mapcontrol(wrapper) {
 			var me = this;
-			this.wrapper = wrapper;
-			this.noLinks = 0;
-			this.templateRow = 0;
+			this.wrapper = wrapper
+		
 
 
 			this.__contruct = function() {
-				me.noLinks = $('tr.links_meta_pair', me.wrapper).length;
-				me.templateRow = $('tr.links_meta_pair', me.wrapper).first().clone();
-				$('input', me.templateRow).val('');
-				
-				// remove-link-meta button
-				$('a.removelink', me.wrapper).click(me.removeLink);
-				
-				// add-link-meta-button
-				$('a#addlink', me.wrapper).click(me.addLink);
+				$(wrapper).vectorMap({
+					map: "de_mill_en",
+				});
 			}
-			
-			
-			this.removeLink = function() {
-				row = $(this).closest('tr.links_meta_pair')
-				row.remove();
-				me.noLinks--;
-				me.renameIDs();
-			}
-			
-			
-			this.addLink = function() {
-				lastPair = $('tr.links_meta_pair', me.wrapper).last();
-					if( $('input', lastPair).last().val() == '') {
-						lastPair.fadeTo(400, 0.2).fadeTo(400, 1.0);
-					} else {
-						newPair = me.templateRow.clone();
-						$('input[name*=_linktext_]', newPair).attr('name', '_linktext_' + me.noLinks);
-  						$('input[name*=_linkurl_]', newPair).attr('name', '_linkurl_' + me.noLinks);
-  						$('a.removelink', newPair).click(me.removeLink);
-						$('tbody', me.wrapper).append(newPair);
-						me.noLinks++;
-					}
-			}
-			
-			
-			this.renameIDs = function() {
-				i = 0;
-				$('tr.links_meta_pair', me.wrapper).each(function(index, value) {
-  					$('input[name*=_linktext_]', value).attr('name', '_linktext_' + i);
-  					$('input[name*=_linkurl_]', value).attr('name', '_linkurl_' + i);
-  					i++;
-  				});
-  				me.noLinks = $('.links_meta_pair', me.wrapper).length;
-			}
-			
 			
 			
 			// call constructor
@@ -67,7 +25,7 @@ jQuery(function($) { klimo_localgroupspage : {
 		
 
 		$(document).ready(function() {
-			var linksControl = new LinksControl('#idea-post-meta-links');
+			var mapsControl = new MapsControl('#groupmap');
 		});
 	}
 
