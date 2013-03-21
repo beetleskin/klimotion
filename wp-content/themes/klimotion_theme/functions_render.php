901146ca1aefd8e118010aa635aec81eb041623a
Stefan Kaiser <stfn@stfn-osx.local>
2013-03-21 22:27:15 +0100

    asdf

--- wp-content/themes/klimotion_theme/functions_render.php
+++ wp-content/themes/klimotion_theme/functions_render.php
@@ -20,8 +20,9 @@ class LocalGroupsPage {
 		wp_register_script('jvectormap-map', get_stylesheet_directory_uri() . '/script/jquery-jvectormap-1.2.2/jquery-jvectormap-de-mill-en.js', array('jquery'));
 	    wp_enqueue_script('jvectormap-map');
 		
-		// add group map style
-		wp_enqueue_style('jvectormap', get_stylesheet_directory_uri() . '/script/jquery-jvectormap-1.2.2/jquery-jvectormap-1.2.2.min.js');
+		// add group map styles
+		wp_enqueue_style('jvectormap', get_stylesheet_directory_uri() . '/script/jquery-jvectormap-1.2.2/jquery-jvectormap-1.2.2.css');
+		wp_enqueue_style('localgroupspage', get_stylesheet_directory_uri() . '/css/localgroupspage.css');
 	}
 	
 	public static function render_map() {
