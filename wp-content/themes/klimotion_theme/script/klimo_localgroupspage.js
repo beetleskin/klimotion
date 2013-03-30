c993960911da41c3291894cab00a2271d021aee5
Marina Thuns <mthuns@Marinas-MacBook-Pro.local>
2013-03-30 15:36:01 +0100

    map mit onclick

--- wp-content/themes/klimotion_theme/script/klimo_localgroupspage.js
+++ wp-content/themes/klimotion_theme/script/klimo_localgroupspage.js
@@ -12,13 +12,36 @@ jQuery(function($) { klimo_localgroupspage : {
 
 
 			this.__contruct = function() {
+				colors = {};
+				colors['1'] = '#A4D886';
+				colors['2'] = '#FCECA2';
+				vals = [];
+				for(var i=0,j=47; i<j; i++){
+					iStr = i.toString();
+				  	vals[iStr] = i;
+				};
+				console.log(vals);
 				$(wrapper).vectorMap({
 					map: "lower_saxony_de",
+					series: { 
+						regions: [
+						{
+							scale: ['#000000','#ffffff'],
+				            attribute: 'fill',
+				            normalizeFunction: 'linear',
+				            values: vals
+			          }]
+					},
+					onRegionClick: function(event, code){
+						alert(code); 
+					}
 				});
+				
 			}
-			//hier alle hinpinselnnnn
+			//jvectormaps style by marina
+			  
 			
-			
+
 			// call constructor
 			this.__contruct();
 		}
