db9dbd15d849a3fac8690c20556751b5a7f9ab73
beetleskin <stfn.ka@gmail.com>
2013-04-01 00:27:13 +0200

    ideaform update

--- wp-content/plugins/klimotion_post_types/klimoPT_idea_form.php
+++ wp-content/plugins/klimotion_post_types/klimoPT_idea_form.php
@@ -5,17 +5,21 @@
 
  
  
-add_action('init', 'NewIdeaForm::init');
- 
 
 class NewIdeaForm {
 
     private $form_action;
     private $form_id;
     private $form_method;
+	private $renderData;
     
-    private static $ioConfig;
-	private static $nonceName = "klimo-newidea";
+    private static $ioConfig = array();
+	private static $nonceName = "klimoIdeaFormNonce";
+	private static $validationConfig = array(
+        'title_max_chars'     	=> 100,
+        'title_min_chars'     	=> 20,
+        'excerp_max_chars'    	=> 200,
+    );
     
 
     function __construct() {
@@ -27,21 +31,62 @@ class NewIdeaForm {
     function preRender() {
         $data = array();
         
+		// defaults
         $nopriv = 'nopriv';
         $submitLink = wp_login_url(get_permalink());
         $onClick = '';
 		$isLoggedIn = get_current_user_id() != 0;
         
+		// login settings
         if($isLoggedIn) {
             $nopriv = '';
             $submitLink = '#';
             $onClick = 'onclick="return false;"';
         }
-        
+		
+		
+		// groups:
+		$groups = get_posts( array( 
+			'post_type' 	=> array('klimo_localGroups'),
+			'post_status'	=> 'publish',
+			'numberposts' 	=> -1
+		));
+		$data['groups'] = array();
+		foreach ($groups as $group) {
+			$data['groups'][] = array("value" => $group->ID, "name" => $group->post_title);
+		}
+
+		// topics
+		$topics = get_terms("klimo_idea_topics", array(
+            'hide_empty'    => false,
+            'hierarchical'  => false,
+            'order_by'      =>'count'
+        ));
+		$data['topics'] = array();
+		foreach ($topics as $topic) {
+			$data['topics'][] = array("value" => $topic->term_id, "name" => $topic->name);
+		}
+		
+		
+		// aims		
+		$aims = get_terms("klimo_idea_aims", array(
+            'hide_empty'    => false,
+            'hierarchical'  => false,
+            'order_by'      =>'count'
+        ));
+		$data['aims'] = array();
+		foreach ($aims as $aim) {
+			$data['aims'][] = array("value" => $aim->term_id, "name" => $aim->name);
+		}
+
+		
+		// leftover
         $data['isLoggedIn'] = $isLoggedIn;
         $data['nopriv'] = $nopriv;
         $data['submitLink'] = $submitLink;
         $data['onClick'] = $onClick;
+		
+		$this->renderData = &$data;
         return $data;
     }
 	
@@ -76,15 +121,18 @@ class NewIdeaForm {
 		      	  	<h1>Idee-Formular</h1>
 		      	  	
 		      	  	<h2>Titel</h2>
-		        	<input type="text" id="idea_title" name="idea_title" placeholder="Gib deiner Idee einen Namen">
+		        	<input type="text" id="idea_title" name="idea_title" placeholder="Gib deiner Idee einen Namen" maxlength="<?php echo self::$validationConfig['title_max_chars'] ?>">
 		        
 			        <h2>Gruppe</h2>
 			        <select id="idea_group" name="idea_group">
-			        	<option value="-1">Keine Gruppe</option>
+			        	<option value="-1" selected="selected">Keine Gruppe</option>
+			        	<?php foreach ( $data['groups'] as &$group ): ?>
+		                    <option value="<?php echo $group['value']; ?>"><?php echo $group['name']; ?></option>
+		                <?php endforeach; ?>
 			        </select>
-			        
+
 			        <h2>Kurze Beschreibung</h2>
-			        <textarea id="idea_excerp" name="idea_excerp" placeholder="Textfeld begrenzt auf 200 Wörter"></textarea>
+			        <textarea id="idea_excerp" name="idea_excerp" placeholder="Textfeld begrenzt auf 200 Wörter" maxlength="<?php echo self::$validationConfig['excerp_max_chars'] ?>"></textarea>
 
 					<h2>Titelbild</h2>
 					<input type="file" id="idea_image" name="idea_image" accept="image/*">
@@ -97,19 +145,33 @@ class NewIdeaForm {
 					<input type="file" id="idea_file1" name="idea_file1">
 					
 					<h2>Weiterführende Links</h2>
-					<input type="text" maxlength="40" name="idea_linktext_1" placeholder="Beschreibung">
-					<input type="text" name="idea_linkurl_1" placeholder="Link">
+					<div id="idea_links">
+						<table>
+							<tbody>
+								<tr class="links_meta_pair">
+									<td><input type="url" maxlength="40" name="linktext_0" placeholder="Beschreibung"></td>
+									<td><input type="url" name="linkurl_0" placeholder="Link"></td>
+									<td><a class="removelink" href="#" onclick="return false;">entfernen</a></td>
+								</tr>
+							</tbody>
+						</table>
+						<a id="addlink" href="#" onclick="return false;">hinzufügen</a>
+					</div>
+					
 					
 					<h2>Thema</h2>
 					<select id="idea_topic" name="idea_topic">
 			        	<?php foreach ( $data['topics'] as &$topic ): ?>
-		                    <option value="<?php echo $topic->term_id ?>"><?php echo $topic->name ?></option>
+		                    <option value="<?php echo $topic['value'] ?>"><?php echo $topic['name'] ?></option>
 		                <?php endforeach; ?>
 			        </select>
 			        
 			        
 			        <h2>Ziele</h2>
-			        <input type="text" id="idea_goals" name="idea_goals" placeholder="Was willst du erreichen?">
+			        <input type="text" id="idea_aim" name="idea_aim">
+			        
+			        
+			        <div class="idea_submit_container"><a href="<?php echo $data['submitLink'] ?>" <?php echo $data['onClick'] ?> id="idea_submit">Abschicken</a></div>
 				</div>	
         	</form>
        </div>
@@ -119,37 +181,127 @@ class NewIdeaForm {
 
     public function printAjaxConfig() {
     	// add security check
-        self::$ioConfig['shoohooooh'] = wp_create_nonce  (self::$nonceName);
+        $ioConfig = self::$ioConfig;
+        $ioConfig[self::$nonceName] = wp_create_nonce  (self::$nonceName);
+		$formData = array(
+			'ajaxConfig' 	=> $ioConfig,
+			'as_groups'		=> $this->renderData['groups'],
+			'as_aims'		=> $this->renderData['aims'],
+		);
         
         // Print data to sourcecode
-        wp_localize_script('klimo_frontend_forms', 'ideaform_config', self::$ioConfig);
+        wp_localize_script('klimo_frontend_forms', 'ideaform_config', $formData);
     }
 
 
     function enqueue_scripts() {
-    	// autosuggest
-        wp_enqueue_script('klimo_frontend_forms', plugins_url('script/klimoPT_frontend_forms.js', __FILE__), array('jquery'));// autosuggest
+    	// frontend forms script
+        wp_enqueue_script('klimo_frontend_forms', plugins_url('script/klimoPT_frontend_forms.js', __FILE__), array('jquery', 'jquery-form'));
+		// autosuggest
+        wp_enqueue_script('autosuggest', plugins_url('script/autoSuggestv14/jquery.autoSuggest.packed.js', __FILE__), array('jquery'));
     }
     
     
     function enqueue_styles() {
+    	// autosuggest style
+        wp_enqueue_style('autosuggest', plugins_url('script/autoSuggestv14/autoSuggest.css', __FILE__));
     }
 
 
-    public static function init() {
-    	self::$ioConfig = array(
-            'ajaxurl'               => admin_url('admin-ajax.php'),
-            'submitAction'          => 'ideaform_submit',
-        );
+    public static function initAjax() {
+    	self::$ioConfig['ajaxurl'] = admin_url('admin-ajax.php');
+		self::$ioConfig['submitAction'] = 'ideaform_submit-ajax.php';
     	
 		// register form ajax
-        add_action('wp_ajax_' . self::$ioConfig['submitAction'], 'IdeaForm::submit');
+        add_action('wp_ajax_' . self::$ioConfig['submitAction'], 'NewIdeaForm::submitHandler');
     }
 
 
-    public static function submit() {
-    	header("Content-Type: text/plain");
-        echo json_encode(array("peter" => "enis"));
+    public static function submitHandler() {
+    	
+		$postData = array();
+		
+		
+		// security check and validations
+		$securityVeto = self::securityCheck($_REQUEST);
+		$validationVeto = self::validate($_REQUEST, $postData);
+		if ( !empty($securityVeto))  {
+			self::ajaxRespond($securityVeto);
+			die();
+		} else if( !empty($validationVeto)) {
+			self::ajaxRespond($validationVeto);
+			die();
+		}
+		
+		
+		
+		// add post
+		// collect values
+		$error = null;
+        $idea_title = $postData['idea_title'];
+		$idea_group_id = $postData['idea_group'];
+		$idea_excerp = $postData['idea_excerp'];
+		$idea_description = $postData['idea_description'];
+		$idea_topic_id = $postData['idea_topic'];
+		$idea_aim_id = $postData['idea_aim'];
+		$idea_links = $postData['idea_links'];
+		// $idea_file1 = wp_strip_all_tags($postData['']);
+		// $idea_image = wp_strip_all_tags($postData['']);
+		
+		
+		
+		$post_args = array(
+            'ping_status'   => 'open',
+            'post_status'   => 'pending',
+            'post_type'     => 'klimo_idea',
+            'post_title'    => $idea_title,
+            'post_content'  => $idea_description,
+            'post_excerpt'  => $idea_excerp,
+            'tax_input'		=> array(
+            	'klimo_idea_topics' => array(intval($idea_topic_id)),
+            	'klimo_idea_aims' => array(intval($idea_aim_id)),
+			),
+        );
+		
+		
+		// create post
+		$postID = wp_insert_post($post_args);
+        if(is_wp_error($postID)) {
+            header("HTTP/1.0 500 Internal Server Error");
+            die();
+        }
+		
+		// save local group meta
+		$group_meta_slug = '_group';
+		if($idea_group_id == -1) {
+			delete_post_meta($postID, $group_meta_slug);
+		} else {
+			update_post_meta($postID, $group_meta_slug, $idea_group_id);
+		}
+		
+		
+		// save link meta
+		$idea_links_meta_slug = '_links';
+		if(!empty($idea_links)){
+			update_post_meta($postID, $idea_links_meta_slug, $idea_links);
+		} 
+		
+		
+		
+		
+		
+		// return
+		$response = array();
+        if($error == null) {
+            $response['success'] = self::createSuccessMessage($postID);
+        } else {
+            $response['error'] = $error;
+        }
+            
+			
+			
+
+        self::ajaxRespond($response);
         die();
     }
 
@@ -158,17 +310,137 @@ class NewIdeaForm {
     }
 
 
-    private static function validate() {
-        return true;
+    private static function validate(&$args, &$postData) {
+    	
+		// check title
+        $element = "idea_title";
+        $value = trim(wp_strip_all_tags($args[$element]));
+        // too short?
+        if(strlen($value) < self::$validationConfig['title_min_chars']) {
+            $response['error'][] = array(
+                'element'   => $element,
+                'message'   => "Der Titel muss mindestens " . self::$validationConfig['title_min_chars'] . " Zeichen lang sein.",
+            );
+        // too long?
+        } else if(strlen($value) > self::$validationConfig['title_max_chars']) {
+            $response['error'][] = array(
+                'element'   => $element,
+                'message'   => "Der Titel darf maximal " . self::$validationConfig['title_max_chars'] . " Zeichen lang sein.",
+            );
+        // check if post is already there
+        } else {
+            $matchingPosts = get_posts(array(
+                'name' => $value,
+                'post_type' => 'klimo_idea',
+                'post_status' => 'publish',
+                'posts_per_page' => 1,)
+            );
+            
+            
+            if($matchingPosts && count($matchingPosts) > 0) {
+                $post = &$matchingPosts[0];
+                $response['error'][] = array(
+                    'element'   => $element,
+                    'message'   => 'Diese Idee gibt es <a href="' . get_post_permalink($post->ID , false) . '" title="' . $post->post_title . '" target="_blank">hier</a> schon!',
+                );
+            }
+        }
+		$postData[$element] = $value;
+		
+		
+		// check group
+		$element = 'idea_group';
+		$value = intval(wp_strip_all_tags($args['idea_group']));
+		$postData[$element] = $value;
+		
+
+		// check excerp
+		$element = 'idea_excerp';
+		$value = trim(wp_strip_all_tags($args[$element]));
+		// too long?
+		if(strlen($value) > self::$validationConfig['excerp_max_chars']) {
+            $response['error'][] = array(
+                'element'   => $element,
+                'message'   => "Kurzbeschreibung darf maximal " . self::$validationConfig['excerp_max_chars'] . " Zeichen lang sein.",
+            );
+        }
+		$postData[$element] = $value;
+		
+		
+		// check description
+        $element = "idea_description";
+        $value = trim(wp_strip_all_tags($args[$element]));
+		$postData[$element] = $value;
+		
+		
+		// check topic
+		$element = 'idea_topic';
+		$value = intval(wp_strip_all_tags($args['idea_topic']));
+		$postData[$element] = $value;
+		
+		
+		// check aim
+		$element = 'idea_aim';
+		$value = intval(wp_strip_all_tags($args['idea_aim']));
+		$postData[$element] = $value;
+		
+		
+		// check links
+		$element = 'idea_links';
+		$value = array();
+		for ($i=0; ;$i++) { 
+			$keyText = 'linktext_' . $i;
+			$keyUrl = 'linkurl_' . $i;
+			if(!array_key_exists ( $keyText , $args ) || !array_key_exists ( $keyUrl , $postData ))
+				break;
+			$valText  = trim(wp_strip_all_tags($args[$keyText]));
+			$valUrl  = trim(wp_strip_all_tags($args[$keyUrl]));
+			
+			if(strlen($valUrl))
+				$value[] = array('text' => $valText, 'url' => $valUrl);
+		}
+		$postData[$element] = $value;
+		
+		
+		return $response;
     }
 
 
-    private static function securityCheck() {
-        return true;
+    private static function securityCheck(&$args) {
+        $response = array();
+        
+        $nonce = NULL;
+        if(key_exists(self::$nonceName, $args)) {
+            $nonce = $args[self::$nonceName];
+        }
+
+        if( !isset($nonce) || wp_verify_nonce($nonce, self::$nonceName) != 1 ) {
+            $response['securityError'] = array(
+                'message'  => '<div id="securityErrorMessage"><p>Sorry, deine Session ist abgelaufen ... </p><a href=".">Neue Idee Schreiben</a></div>',
+                'args' => $args
+            );
+        }
+        
+        return $response;
     }
     
 	
-    private static function sumitSuccessHTML($postID) {
+    private static function createSuccessMessage($postID) {
+		$postPermaLink = get_post_permalink($postID, false);
+        
+        $html = '<div id="submitSuccessMessage">';
+        $html .= '<p>Deine Idee wurde erfoglreich abgeschickt und wird von uns geprüft.</p>';
+        $html .= '<div class="redirect thePost"><a href="' . $postPermaLink . '">Idee ansehen</a></div>';
+        $html .= '<div class="redirect newIdea"><a href=".">Neue Idee Schreiben</a></div>';
+        $html .= '</div>';
+        
+        return $html;
+    }
+	
+	public static function ajaxRespond(&$message) {
+		header("Content-Type: text/plain");
+        echo json_encode($message);
+        die();
     }
 }
 ?>
--- wp-content/plugins/klimotion_post_types/script/klimoPT_frontend_forms.js
+++ wp-content/plugins/klimotion_post_types/script/klimoPT_frontend_forms.js
@@ -3,15 +3,221 @@
  */
 
 jQuery(function($) { ideaform : {
-
-
-		function IdeaForm(wrapper) {
+	
+	
+		function LinksControl(wrapper) {
+			// TODO: this class is a exact copy of the one in klimoPT_admin.js ... merge!
 			var me = this;
+			this.wrapper = wrapper;
+			this.noLinks = 0;
+			this.templateRow = 0;
+			this.textSelect = 'linktext_';
+			this.urlSelect = 'linkurl_'
+			
 
 
 			this.__contruct = function() {
-				console.log(ideaform_config)
+				me.noLinks = $('tr.links_meta_pair', me.wrapper).length;
+				me.templateRow = $('tr.links_meta_pair', me.wrapper).first().clone();
+				$('input', me.templateRow).val('');
+				
+				// remove-link-meta button
+				$('a.removelink', me.wrapper).click(me.removeLink);
+				
+				// add-link-meta-button
+				$('a#addlink', me.wrapper).click(me.addLink);
 			}
+			
+			
+			this.removeLink = function() {
+				row = $(this).closest('tr.links_meta_pair')
+				row.remove();
+				me.noLinks--;
+				me.renameIDs();
+			}
+			
+			
+			this.addLink = function() {
+				lastPair = $('tr.links_meta_pair', me.wrapper).last();
+				if( $('input', lastPair).last().val() == '') {
+					lastPair.fadeTo(400, 0.2).fadeTo(400, 1.0);
+				} else {
+					newPair = me.templateRow.clone();
+					$('input[name*=' + me.textSelect + ']', newPair).attr('name', me.textSelect + me.noLinks);
+					$('input[name*=' + me.urlSelect + ']', newPair).attr('name', me.urlSelect + me.noLinks);
+					$('a.removelink', newPair).click(me.removeLink);
+					$('tbody', me.wrapper).append(newPair);
+					me.noLinks++;
+				}
+			}
+			
+			
+			this.renameIDs = function() {
+				i = 0;
+				$('tr.links_meta_pair', me.wrapper).each(function(index, value) {
+  					$('input[name*=' + me.textSelect + ']', value).attr('name', me.textSelect + i);
+  					$('input[name*=' + me.urlSelect + ']', value).attr('name', me.urlSelect + i);
+  					i++;
+  				});
+  				me.noLinks = $('.links_meta_pair', me.wrapper).length;
+			}
+			
+			
+			
+			// call constructor
+			this.__contruct();
+		}
+
+		
+		function IdeaForm(wrapper) {
+			var me = this;
+			this.wrapper = $(wrapper);
+			this.form = $('form', me.wrapper);
+			this.submit = $('a#idea_submit', wrapper);
+			this.linksControl = null;
+
+
+			this.__contruct = function() {
+				me.config = ideaform_config;
+				me.linksControl = new LinksControl($("#idea_links", wrapper));
+				
+				
+				// aims autosuggest
+				$('input#idea_aim').autoSuggest(me.config.as_aims, {
+					selectedItemProp : "name",
+					searchObjProps : "name",
+					minChars : 1,
+					startText : "Ziele ...",
+					emptyText : "neues Ziel mit TAB",
+				});
+				
+				
+				
+				// declare ajaxForm
+				var formOptions = {
+					beforeSubmit : me.beforeSubmit,
+					success : me.successHandler,
+					error : me.transmissionErrorHandler,
+					url : me.config.ajaxConfig.ajaxurl,
+					dataType : "json",
+					data : {
+						'action' : me.config.ajaxConfig.submitAction,
+						'klimoIdeaFormNonce' : me.config.ajaxConfig.klimoIdeaFormNonce
+					},
+
+				};
+				
+				me.form.ajaxForm(formOptions);
+				me.submit.click(function() {
+					me.form.submit();
+					return false;
+				});
+			}
+			
+			this.beforeSubmit = function(formData, jqForm, options) {
+			}
+			
+			this.successHandler = function(response, statusText, xhr, $form) {
+				
+				// error handling
+				if(response.error != null) {
+					me.errorHandler(response.error);
+				} else if(response.securityError != null) {
+					me.securityErrorHandler(response.securityError);
+				} else if(response.success != null) {
+					me.submitSuccessHandler(response);
+				} else {
+					alert("xxx this should never happen!")
+				}
+			}
+			
+			this.transmissionErrorHandler = function(param) {
+				return;
+				me.form.animate({
+					opacity : 'toggle',
+					height : 'toggle'
+				}, "slow", function(event) {
+					var errorDiv = $('<div id="connectionerror"><p>Hoppla, da ist was schief gelaufen. Bitte versuch es später noch einmal.</p></div>');
+					errorDiv.css('display', 'none');
+					me.form.replaceWith(errorDiv);
+					errorDiv.animate({
+						opacity : 'toggle',
+						height : 'toggle'
+					}, "slow");
+				});
+
+				setTimeout("location.reload()", 5000);
+			}
+			
+			this.errorHandler = function(errors) {
+				// remove all errors
+				$('.formerror', me.form).removeClass('formerror');
+
+				$('#errormessage', me.form).animate({
+					opacity : 'toggle',
+					height : 'toggle'
+				}, 600, function(event) {
+					$('#errormessage', me.form).empty();
+					
+					for (var e=0; e < errors.length; e++) {
+						if(errors[e].element != null && errors[e].message != null) {
+							// display error message
+							$('#errormessage', me.form).append('<p>' + errors[e].message + '</p>');
+							// add css class to errorous element
+							$('[name="' + errors[e].element + '"]', me.form).addClass('formerror');
+						}
+					}
+
+					if(errors.length > 0) {
+						$('#errormessage', me.form).animate({
+							opacity : 'toggle',
+							height : 'toggle'
+						}, "slow");
+					}
+				});
+			}
+			
+			
+			this.securityErrorHandler = function(securityError) {
+				if(securityError.message != null) {
+					me.form.animate({
+						opacity : 'toggle',
+						height : 'toggle'
+					}, "slow", function(event) {
+						var errorDiv = $(securityError.message);
+						errorDiv.css('display', 'none');
+
+						me.form.replaceWith(errorDiv);
+						errorDiv.animate({
+							opacity : 'toggle',
+							height : 'toggle'
+						}, "slow");
+					});
+
+					setTimeout("location.reload()", 5000);
+				} else {
+					// fallback strategy ...
+					location.reload();
+				}
+			}
+			
+			this.submitSuccessHandler = function(response) {
+				me.form.animate({
+					opacity : 'toggle',
+					height : 'toggle'
+				}, "slow", function(event) {
+					var successDiv = $(response.success);
+					successDiv.css('display', 'none');
+
+					me.form.replaceWith(successDiv);
+					successDiv.animate({
+						opacity : 'toggle',
+						height : 'toggle'
+					}, "slow");
+				});
+			}
+			
+			
 			
 			
 			// call constructor
@@ -21,7 +227,7 @@ jQuery(function($) { ideaform : {
 		
 
 		$(document).ready(function() {
-			var ideaForm = new IdeaForm('body');
+			var ideaForm = new IdeaForm('#ideaform_wrap');
 		});
 	}
 
