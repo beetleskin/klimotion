2d84f087683e9720c0650e818125009a59d575d9
Marina Thuns <mthuns@Marinas-MacBook-Pro.local>
2013-04-28 19:39:48 +0200

    ales nue

--- wp-content/themes/klimotion_theme/searchform.php
+++ wp-content/themes/klimotion_theme/searchform.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * The template for displaying search forms in Cazuela
- *
+ *TODO: Suchtext in Übersetzung eintragen!
  * @package Cazuela
  * @since Cazuela 1.0
  */
 ?>
 
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<label for="s" class="assistive-text"><?php _e( 'Search', 'cazuela' ); ?></label>
-	<input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search&hellip;', 'cazuela' ); ?>" />
+	<label for="s" class="assistive-text"><?php _e( 'Suche', 'cazuela' ); ?></label>
+	<input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Suche&hellip;', 'cazuela' ); ?>" />
 	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Suche' ); ?>" />
 </form>
