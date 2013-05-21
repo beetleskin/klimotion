7a2ae1b9dc08895491aa6c94fa2f819eea396f25
beetleskin <stfn.ka@gmail.com>
2013-05-21 18:28:16 +0200

    fixed https://github.com/beetleskin/klimotion/issues/86

--- wp-content/themes/klimotion_theme/post-footer.php
+++ wp-content/themes/klimotion_theme/post-footer.php
@@ -8,36 +8,44 @@
  */
 ?>
 
-<footer class="entry-meta">
-	<div class="entry-cats-tags">
-		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( __( ', ', 'cazuela' ) );
-			if ( $categories_list && thsp_categorized_blog() ) :
-		?>
+<?php 
+
+	/* get data to be rendered */
+	$categories_list = get_the_category_list( __( ', ', 'cazuela' ) );
+	$tags_list = get_the_tag_list( '', __( ', ', 'cazuela' ) );
+	$renderCommentsLink = ( !post_password_required() && ( comments_open() || '0' != get_comments_number() ) );
+	$userCanEditPost = user_can_edit_post(get_current_user_id(), $post->ID);
+	
+?>
+
+<?php if ( !empty($categories_list) || !empty($tags_list) || $renderCommentsLink || $userCanEditPost ) : ?>
+	
+	<footer class="entry-meta">
+		<div class="entry-cats-tags">
+			<?php if ( $categories_list ) : ?>
+				<span class="cat-links">	
+					<?php printf( __( 'Erstellt in %1$s' ), $categories_list ); ?>
+				</span>
+				<span class="sep"> | </span>
+			<?php endif; // End if categories ?>
 		
-		<span class="cat-links">	
-			<?php printf( __( 'Erstellt in %1$s' ), $categories_list ); ?>
-		</span>
-		<span class="sep"> | </span>
-		<?php endif; // End if categories ?>
+			<?php if ( $tags_list ) : ?>
+				<span class="tags-links">
+					<?php printf( __( 'Tagged %1$s', 'cazuela' ), $tags_list ); ?>
+				</span>
+				<span class="sep"> | </span>
+			<?php endif; // End if tags ?>
+		
+			<?php if ( $renderCommentsLink ) : ?>
+				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment' ), __( '1 Comment' ), __( '% Comments') ); ?></span>
+			<?php endif; ?>
+		</div><!-- .entry-cats-tags -->
 	
-		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', __( ', ', 'cazuela' ) );
-			if ( $tags_list ) :
-		?>
-		<span class="tags-links">
-			<?php printf( __( 'Tagged %1$s', 'cazuela' ), $tags_list ); ?>
-		</span>
-		<span class="sep"> | </span>
-		<?php endif; // End if $tags_list ?>
 	
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment' ), __( '1 Comment' ), __( '% Comments') ); ?></span>
-		<?php endif; ?>
-	</div><!-- .entry-cats-tags -->
+		<?php edit_post_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-meta -->
+	
+<?php endif; // End if render_footer ?>
 
 
-	<?php edit_post_link( __( 'Edit' ), '<span class="edit-link">', '</span>' ); ?>
-</footer><!-- .entry-meta -->
+
